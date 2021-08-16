e102ba60399d9e21a64663a721ef740488bfa9c2
VaguerCrown3732 <88161102+VaguerCrown3732@users.noreply.github.com>
2021-08-16 11:04:50 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,20 +13,20 @@ function setup(){
   
 sea=createSprite(400,200);
 sea.addImage(seaImg);
-sea.velocityX = -5;
 sea.scale=0.3;
   
 ship = createSprite(130,200,30,30);
 ship.addAnimation("movingShip",shipImg1);
-ship.scale =0.25;
+ship.scale = 0.25;
 }
 function draw(){
   //set background color 
   background(0);
-  sea.velocityX = -3;
+  sea.velocityX = 4;
+  
 
-  if(sea.x < 0){
-    sea.x = sea.width/8;
+  if(sea.x > 200){
+    sea.x = - 200;
   }
   
   drawSprites();
